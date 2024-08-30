5705fdb58aaedf0c498e41379e8b9829b4a65d32
lagigliaivan <lagigliaivan@gmail.com>
2024-08-30 11:52:33 -0300

    changing names

--- properrors/errors.go
+++ properrors/errors.go
@@ -3,27 +3,27 @@ package properrors
 import "fmt"
 
 var (
-	ErrFailedToLogin    = NewProperError("0001", "Failed to login")
-	ErrAccountNotFound  = NewProperError("0002", "Account not found")
-	ErrSecondFactorAuth = NewProperError("0003", "Error en login por second factor")
+	ErrFailedToLogin    = New("0001", "Failed to login")
+	ErrAccountNotFound  = New("0002", "Account not found")
+	ErrSecondFactorAuth = New("0003", "Error en login por second factor")
 )
 
 type (
-	ProperError struct {
+	Error struct {
 		ID   string `json:"id"`
 		Err  string `json:"error"`
 		Desc string `json:"description"`
 	}
 )
 
-func NewProperError(id string, err string) *ProperError {
-	return &ProperError{
+func New(id string, err string) *Error {
+	return &Error{
 		ID:   id,
 		Desc: "https://proper.com/errors/" + id,
 		Err:  err,
 	}
 }
 
-func (p *ProperError) Error() string {
+func (p *Error) Error() string {
 	return fmt.Sprintf("%s: %s desc:%s", p.ID, p.Err, p.Desc)
 }
